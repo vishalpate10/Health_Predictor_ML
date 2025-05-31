2a37232c709ced6ef3f85578124c283b4d2baf5d
Vishal pate <vprakashpate@gmail.com>
2025-05-31 19:09:28 +0530

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url('https://cdn.pixabay.com/photo/2017/02/08/12/51/heart-2048384_1280.jpg');
+        background-image: url('pic.png');
         background-size: cover;
         background-position: center;
         color: black;
